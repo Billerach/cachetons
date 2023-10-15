6a5160cbf97f0e0dd4cf4dc258162ada2deee607
Pascal Billerach p.billerach@gmail.com <p.billerach@gmail.com>
2023-10-15 17:19:41 +0200

    Devise installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get 'company_links/create'
+  root to: "home#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :companies do
+    resources :payslips, except: %i[update destroy]
+    patch '/payslips/:id', to: 'payslips#update', as: :payslip_update
+    delete '/payslips/:id', to: 'payslips#destroy', as: :payslip_destroy
+    resources :company_links, only: [:create]
+    resources :employees, except: [:index]
+    resources :performances, except: %i[index new show]
+  end
+
+  resources :job_profiles
+  resources :contributions
+
+  get 'favicon.ico', to: 'application#favicon'
 end
