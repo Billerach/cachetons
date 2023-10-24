99635d398926a90bbd948771dbf18e47f2ad7433
Billerach <p.billerach@gmail.com>
2023-10-24 22:42:35 +0200

    prod config - config.hosts added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << 'www.cachetons.com'
+
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = false #PENSER A REMETTRE A TRUE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
