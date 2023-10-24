b7af3e6c74f9d69bcf0c553e4fea36503ffde961
Billerach <p.billerach@gmail.com>
2023-10-25 00:50:45 +0200

    prod config - back to initial state

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << 'localhost'
-  config.hosts << "www.cachetons.com"
-  config.hosts << "cachetons.com"
-  config.hosts << "skalpa.skilledskulls.com"
-
   # Code is not reloaded between requests.
   config.cache_classes = false #PENSER A REMETTRE A TRUE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
