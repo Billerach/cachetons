ac9966def8a9f6e6462244067803e6e50f645d11
Billerach <p.billerach@gmail.com>
2023-11-19 16:04:40 +0100

    Companies index CSS - WIP

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { Application } from "stimulus"
+window.Stimulus = Application.start()
+
+import AddCompanyController from "./controllers/add_company_controller.js"
+Stimulus.register("add-company-button", AddCompanyController)
