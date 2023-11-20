14510b63d4dbe452672a8c00a147ea3f82e92d3a
Pascal Billerach p.billerach@gmail.com <p.billerach@gmail.com>
2023-11-20 11:50:12 +0100

    Schemas modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_13_204259) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_13_200741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
