85a180dd1e8c9939e3c0f877ea4669e55e1ee844
Pascal Billerach p.billerach@gmail.com <p.billerach@gmail.com>
2024-01-24 15:12:21 +0100

    fixing notice display bugs js

--- app/javascript/controllers/notice_controller.js
+++ app/javascript/controllers/notice_controller.js
@@ -2,6 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="notice"
 export default class extends Controller {
+  static targets = ["closeNoticeButton"]
+
   connect() {
+  }
+
+  close() {
+    this.closeNoticeButtonTarget.remove();
+    const blackout = document.getElementById("blackout");
+    blackout.remove();
   }
 }
